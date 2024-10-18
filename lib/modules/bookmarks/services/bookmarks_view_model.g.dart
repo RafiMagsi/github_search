e93da27fd47436de5378eab75e4e9982629b590c
Rafi Magsi <muhammed.rafi@taifco.net>
2024-10-18 12:19:46 +0400

    - Code optimizations

---
+++ lib/modules/bookmarks/services/bookmarks_view_model.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bookmarks_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$bookmarksNotifierHash() => r'bb6a15544418af9e5b450c8b0039e72207d17896';
+
+/// See also [BookmarksNotifier].
+@ProviderFor(BookmarksNotifier)
+final bookmarksNotifierProvider = AutoDisposeNotifierProvider<BookmarksNotifier,
+    List<GitRepository>>.internal(
+  BookmarksNotifier.new,
+  name: r'bookmarksNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$bookmarksNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BookmarksNotifier = AutoDisposeNotifier<List<GitRepository>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
